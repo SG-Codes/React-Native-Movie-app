4d7f30c3dfab9c5d3368d44a9e4335a80eb9c44c
vngarg <sngargrsd@gmail.com>
2021-11-28 02:42:07 +0530

    Added movie app

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Constants from './src/Constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MovieDetails from './src/Components/MovieDetails';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Stack.Screen name="Home" component={Home} options={headerStyle} />
+        <Stack.Screen
+          name="movieDetails"
+          component={MovieDetails}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const headerStyle = {
+  title: 'Movies',
+  headerStyle: {backgroundColor: Constants.baseColor},
+  headerTitleStyle: {color: Constants.textColor},
+  headerLeft: () => <Icon name="menu" size={34} color={Constants.textColor} />,
+  headerRight: () => (
+    <Icon name="search" size={25} color={Constants.textColor} />
+  ),
+};
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'ADD YOUR API KEY HERE';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_POSTER_URL = 'https://image.tmdb.org/t/p/original';
+export const POSTER_IMAGE = 'https://image.tmdb.org/t/p/w200';
